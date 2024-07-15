d14b181460face7212128111b911c28e833b59f3
shyam <shyam.bista3210@gmail.com>
2024-07-15 14:40:08 +0545

    fecting data from backend

--- frontend/lib/screens/movielist.dart
+++ frontend/lib/screens/movielist.dart
@@ -1,23 +1,126 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class Movielist extends StatefulWidget {
-  const Movielist({super.key});
+  const Movielist({Key? key}) : super(key: key);
 
   @override
   State<Movielist> createState() => _MovielistState();
 }
 
 class _MovielistState extends State<Movielist> {
+  List<dynamic> movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+  Future<void> fetchMovies() async {
+    final response = await http.get(Uri.parse('http://localhost:8080/api/movies'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        movies = json.decode(response.body);
+      });
+    } else {
+      // Handle errors
+      print('Failed to load movies');
+    }
+  }
+
+  Future<void> deleteMovie(int id) async {
+    final apiUrl = 'http://localhost:8080/api/movies/$id';
+
+    final response = await http.delete(Uri.parse(apiUrl));
+
+    //TODO: The status code should be 200 but this is giving 204, should fixed in the backend
+    if (response.statusCode == 204) {
+      // If deletion is successful, reload the movie list
+      await fetchMovies();
+    } else {
+      // Handle errors
+      print('Failed to delete movie');
+    }
+  }
+
+  Future<void> confirmDeleteDialog(int id) async {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Delete'),
+          content: Text('Are you sure you want to delete this movie?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(
+                'Delete',
+                style: TextStyle(color: Colors.red),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                deleteMovie(id);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Movie WatchList",
-        style: TextStyle(color: Colors.white)),
+        title: const Text("Movie WatchList"),
         backgroundColor: Colors.greenAccent[400],
-        centerTitle: true
+        centerTitle: true,
       ),
-      body: const Text("No Movies!!!"),
+      body: movies.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  elevation: 3,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: ListTile(
+                    title: Text(
+                      movies[index]['title'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Genre: ${movies[index]['genre']}'),
+                        Text('Watched: ${movies[index]['watched'] ? 'Yes' : 'No'}'),
+                        if (movies[index]['watchDate'] != null)
+                          Text('Watch Date: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['watchDate']))}'),
+                        if (movies[index]['createdAt'] != null)
+                          Text('Created At: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['createdAt']))}'),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        confirmDeleteDialog(movies[index]['id']);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
