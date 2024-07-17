992d3a52b77d14c632dc4f7f11807c54f57e6395
shyam <shyam.bista3210@gmail.com>
2024-07-17 18:46:29 +0545

    delete movie and code formatting

--- frontend/lib/screens/movielist.dart
+++ frontend/lib/screens/movielist.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
 class Movielist extends StatefulWidget {
-  const Movielist({Key? key}) : super(key: key);
+  const Movielist({super.key});
 
   @override
   State<Movielist> createState() => _MovielistState();
@@ -19,9 +19,60 @@ class _MovielistState extends State<Movielist> {
     fetchMovies();
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Movie WatchList"),
+        backgroundColor: Colors.greenAccent[400],
+        centerTitle: true,
+      ),
+      body: movies.isEmpty
+          ? const Center(
+              child: Text("No movies"),
+            )
+          : ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  elevation: 3,
+                  margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  child: ListTile(
+                    title: Text(
+                      movies[index]['title'],
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Genre: ${movies[index]['genre']}'),
+                        Text(
+                            'Watched: ${movies[index]['watched'] ? 'Yes' : 'No'}'),
+                        if (movies[index]['watchDate'] != null)
+                          Text(
+                              'Watch Date: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['watchDate']))}'),
+                        if (movies[index]['createdAt'] != null)
+                          Text(
+                              'Created At: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['createdAt']))}'),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        confirmDeleteDialog(movies[index]['id']);
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+    );
+  }
+
   Future<void> fetchMovies() async {
     // final response = await http.get(Uri.parse('http://192.168.1.64:8080/api/movies'));
-    final response = await http.get(Uri.parse('http://localhost:8080/api/movies')); 
+    final response =
+        await http.get(Uri.parse('http://localhost:8080/api/movies'));
 
     if (response.statusCode == 200) {
       setState(() {
@@ -43,27 +94,27 @@ class _MovielistState extends State<Movielist> {
       // If deletion is successful, reload the movie list
       await fetchMovies();
     } else {
-      // Handle errors
+      // TODO: Handle errors
       print('Failed to delete movie');
     }
   }
-  
+
   Future<void> confirmDeleteDialog(int id) async {
     return showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Confirm Delete'),
-          content: Text('Are you sure you want to delete this movie?'),
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this movie?'),
           actions: <Widget>[
             TextButton(
-              child: Text('Cancel'),
+              child: const Text('Cancel'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: Text(
+              child: const Text(
                 'Delete',
                 style: TextStyle(color: Colors.red),
               ),
@@ -77,51 +128,4 @@ class _MovielistState extends State<Movielist> {
       },
     );
   }
-
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      title: const Text("Movie WatchList"),
-      backgroundColor: Colors.greenAccent[400],
-      centerTitle: true,
-    ),
-    body: movies.isEmpty
-        ? Center(
-            child: Text("No movies"), // Show text when movies list is empty
-          )
-        : ListView.builder(
-            itemCount: movies.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Card(
-                elevation: 3,
-                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: ListTile(
-                  title: Text(
-                    movies[index]['title'],
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Genre: ${movies[index]['genre']}'),
-                      Text('Watched: ${movies[index]['watched'] ? 'Yes' : 'No'}'),
-                      if (movies[index]['watchDate'] != null)
-                        Text('Watch Date: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['watchDate']))}'),
-                      if (movies[index]['createdAt'] != null)
-                        Text('Created At: ${DateFormat('dd/MM/yyyy').format(DateTime.parse(movies[index]['createdAt']))}'),
-                    ],
-                  ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () {
-                      confirmDeleteDialog(movies[index]['id']);
-                    },
-                  ),
-                ),
-              );
-            },
-          ),
-  );
-}
 }
